70d1159ede3f3fcb548db9ecac0f5d33b39633a3
markoptionb <optionbsupport@gmail.com>
2021-03-11 17:19:24 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName.toUpperCase() 
+};
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+};
+
+const leastFavoriteCustomer = 'Anthony'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'john'
+    return leastFavoriteCustomer
+};
+
